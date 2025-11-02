47a721320c9dadd9042c1a06a277b36e65a2437a
Sushanth <your-email@example.com>
2025-11-03 01:23:14 +0530

    Github Initiated

---
+++ server/controllers/post.controller.js
@@ -0,0 +1,63 @@
+import User from '../models/user.model.js';
+import Post from '../models/post.model.js';
+
+
+export const getPosts = async (req, res) => {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+}
+
+export const getPost = async (req, res) => {
+    const post = await Post.findOne({slug: req.params.slug});
+    res.status(200).json(post);
+}
+
+export const createPost = async (req, res) => {
+
+    const clerkUserId = req.auth.userId;
+    if(!clerkUserId){
+        return res.status(401).json("Unauthorized");
+    }
+
+    const user = await User.findOne({clerkUserId});
+
+    if(!user){
+        return res.status(404).json("User not found");
+    }
+
+    let slug = req.body.title.replace(/ /g, "-").toLowerCase()
+
+    let existingPost = await Post.findOne({ slug })
+
+    let counter = 2;
+
+    while(existingPost){
+        slug = `${slug}-${counter}`
+        existingPost = await Post.findOne ({ slug });
+        counter++;
+    }
+
+    const newPost = new Post({user:user._id, slug, ...req.body});
+
+    const post = await newPost.save();
+    res.status(200).json(post);
+}
+
+export const deletePost = async (req, res) => {
+
+     const clerkUserId = req.auth.userId;
+    if(!clerkUserId){
+        return res.status(401).json("Unauthorized");
+    }
+
+    const user = await User.findOne({clerkUserId});
+
+
+    const deletedPost = await Post.findByIdAndDelete({_id:req.params.id, user:user._id});
+
+    if(!deletedPost){
+        return res.status(403).json("You can delete only your posts");
+    }
+
+    res.status(200).json("Post deleted successfully");
+}
---
+++ server/index.js
@@ -0,0 +1,55 @@
+import express from "express";
+import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js";
+import commentRouter from "./routes/comment.route.js";
+import connectDB from "./lib/connectDB.js";
+import dotenv from "dotenv";
+import webHookRouter from "./routes/webhook.route.js";
+import { clerkMiddleware} from "@clerk/express";
+import { requireAuth } from "@clerk/clerk-sdk-node";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors(process.env.CLIENT_URL));
+app.use(clerkMiddleware());
+app.use("/webhooks", webHookRouter);
+app.use(express.json());
+
+// app.get("/protect", (req, res)=>{
+//     const {userId} = req.auth;
+//     if(!userId){
+//         return res.status(401).json("Unauthorized");
+//     }
+//     res.status(200).json("You are authorized");
+// });
+
+// app.get("/protect", requiredAuth(), (req, res)=>{
+//     res.status(200).json("You are authorized");
+// });
+
+
+
+app.use("/users", userRouter);
+app.use("/posts", postRouter); 
+app.use("/comments", commentRouter);
+
+
+
+app.use((error, req, res, next)=>{
+
+    res.status(error.status || 500);
+
+    res.json({message: error.message || "Sonething went wrong!",
+    status: error.status,
+    stack: error.stack,
+
+    });
+})
+
+app.listen(3000, ()=>{
+    connectDB();
+    console.log("server is running!")
+})
